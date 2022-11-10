9ef759c72dcdb44998831f3a0aa68ee404a5c40a
T30G10v4 <matteo.giovagnotti@gmail.com>
2022-11-10 15:34:55 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<script>
+
+    import axios from "axios";
+    import AppWrapper from "./components/AppWrapper.vue";
+    import AppCharacter from "./components/AppCharacter.vue";
+    import AppCharactersList from "./components/AppCharactersList.vue";
+    import { cards } from "./cards";
+
+    export default {
+
+        components: {
+
+            AppWrapper,
+            AppCharacter,
+            AppCharactersList
+
+        },
+        data() {
+
+            return {
+
+                cards
+
+            }
+
+        },
+        created() {
+
+            this.cards.loading = true;
+            axios.get("https://www.breakingbadapi.com/api/characters").then((resp) => {
+
+                
+                this.cards.characters = resp.data;
+                console.log(this.cards.characters);
+                this.cards.loading = false;
+
+            });
+
+        }
+
+    }
+
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss">
+    @use "./styles/generals.scss" as *;
+</style>
---
+++ src/cards.js
@@ -0,0 +1,6 @@
+import { reactive } from "vue";
+
+export const cards = reactive({
+  characters: [],
+  loading: false,
+});
